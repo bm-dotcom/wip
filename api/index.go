56b82a57ddaf15da62ab47a855a7092a511cc761
bm-dotcom <BuzzManage@proton.me>
2025-11-21 21:57:56 -0500

    vercel

--- api/index.go
+++ api/index.go
@@ -1,55 +1,10 @@
 package handler
-
+ 
 import (
-	"embed"
-	"html/template"
-	"io/fs"
-	"net/http"
-	"strings"
+  "fmt"
+  "net/http"
 )
-
-//go:embed ../templates/*
-var templateFS embed.FS
-
-var templates *template.Template
-
-func init() {
-	subFS, err := fs.Sub(templateFS, "templates")
-	if err != nil {
-		panic(err)
-	}
-	templates = template.Must(template.ParseFS(subFS, "*.html"))
-}
-
+ 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	// Clean the path
-	p := strings.Trim(r.URL.Path, "/")
-	if p == "" {
-		p = "index"
-	}
-
-	var tmplName string
-	switch p {
-	case "index", "":
-		tmplName = "index.html"
-	case "about":
-		tmplName = "about.html"
-	default:
-		http.NotFound(w, r)
-		return
-	}
-
-	// Fix for the syntax error: no ternary in Go!
-	title := "Home"
-	if p != "" && p != "index" {
-		title = strings.Title(p)
-	}
-
-	err := templates.ExecuteTemplate(w, tmplName, map[string]string{
-		"Title": title,
-	})
-	if err != nil {
-		http.Error(w, "Template error: "+err.Error(), http.StatusInternalServerError)
-		return
-	}
+  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
 }
