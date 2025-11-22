d64a35db98aea8586d2c2a1b644dcf2ab7fa0b31
bm-dotcom <BuzzManage@proton.me>
2025-11-21 20:47:28 -0500

    changes to index

--- api/index.go
+++ api/index.go
@@ -1,10 +1,47 @@
 package handler
- 
+
 import (
-  "fmt"
-  "net/http"
+	"embed"
+	"fmt"
+	"html/template"
+	"net/http"
+	"strings"
 )
- 
+
+// Embed all templates at build time (no filesystem access needed at runtime)
+//
+//go:embed templates/*
+var templates embed.FS
+
+var tmpl *template.Template
+
+// Parse once at cold start – cheap and safe in serverless
+func init() {
+	var err error
+	tmpl, err = template.New("").ParseFS(templates, "templates/*.html")
+	if err != nil {
+		panic(err)
+	}
+}
+
 func Handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	path := r.URL.Path
+	if path == "" || path == "/" {
+		path = "/index.html"
+	} else {
+		path = "/" + strings.TrimPrefix(path, "/") + ".html"
+		if path == "/about.html" { // only allow the pages you actually have
+			// ok
+		} else if path != "/index.html" {
+			http.NotFound(w, r)
+			return
+		}
+	}
+
+	// If you use a layout/base template, change the name below accordingly
+	err := tmpl.ExecuteTemplate(w, strings.TrimPrefix(path, "/"), nil)
+	if err != nil {
+		http.Error(w, "Template error", http.StatusInternalServerError)
+		fmt.Println("template error:", err)
+	}
 }
