5260dd75943a02874344c78162f6a13106e6c9ee
bm-dotcom <BuzzManage@proton.me>
2025-11-21 19:44:42 -0500

    vercel fix

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ package main
 
 import (
 	"log"
+	"os"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -26,43 +27,49 @@ import (
 func main() {
 
 	/*
-	TEMPLATE ENGINE SETUP
-	Fiber uses HTML templates to render dynamic content. The template engine:
-	- Looks for template files in ./templates/ directory
-	- Uses .html file extension
-	- Supports Go template syntax with {{.VariableName}} notation
+		TEMPLATE ENGINE SETUP
+		Fiber uses HTML templates to render dynamic content. The template engine:
+		- Looks for template files in ./templates/ directory
+		- Uses .html file extension
+		- Supports Go template syntax with {{.VariableName}} notation
 	*/
 	engine := html.New("./templates", ".html")
 
 	/*
-	FIBER APPLICATION SETUP
-	Creates a new Fiber web application instance with configuration.
-	Fiber is similar to Express.js but written in Go for high performance.
+		FIBER APPLICATION SETUP
+		Creates a new Fiber web application instance with configuration.
+		Fiber is similar to Express.js but written in Go for high performance.
 	*/
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
 
 	/*
-	ROUTE DEFINITIONS
-	Here we define all the URL routes that our web server responds to.
-	Each route connects a URL path to a handler function.
+		ROUTE DEFINITIONS
+		Here we define all the URL routes that our web server responds to.
+		Each route connects a URL path to a handler function.
 	*/
 
 	// HOMEPAGE ROUTE: GET request to "/" (root URL)
 	// This is the main page visitors see when they visit the website
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("index", fiber.Map{
-			"Title":    "My Go Webpage",
-			"Message":  "Welcome to my simple Go webpage!",
-			"Time":     time.Now().Format("2006-01-02 15:04:05"),
+			"Title":   "My Go Webpage",
+			"Message": "Welcome to my simple Go webpage!",
+			"Time":    time.Now().Format("2006-01-02 15:04:05"),
 		})
 	})
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000" // local dev
+	}
+	log.Fatal(app.Listen(":" + port)) // ← change from hardcoded :3000
+
 	/*
-	SERVER STARTUP
-	Start the web server and begin accepting HTTP requests.
-	The server will run indefinitely until stopped with Ctrl+C.
+		SERVER STARTUP
+		Start the web server and begin accepting HTTP requests.
+		The server will run indefinitely until stopped with Ctrl+C.
 	*/
 
 	log.Printf("🚀 Server starting on http://localhost:8080")
